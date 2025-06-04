fac286c69072f46b7d5c7c702f7c534dde7eaee0
abubdcalling <abubdcalling@gmail.com>
2025-06-04 15:35:41 +0600

    add

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,10 @@ Route::post('login', [AuthController::class, 'login']);
 Route::get('me', [AuthController::class, 'me'])->middleware('auth:api');
 Route::post('logout', [AuthController::class, 'logout'])->middleware('auth:api');
 
+Route::post('password/email', [AuthController::class, 'sendResetOTP']);
+Route::post('password/verify-otp', [AuthController::class, 'verifyResetOTP'])->name('password.verify-otp');
+Route::post('password/reset', [AuthController::class, 'passwordReset'])->name('password.reset');
+
 Route::resource('posts', PostController::class);
 
 
